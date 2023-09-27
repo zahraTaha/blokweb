4f1efcafdbc81651ea29524e03f6704936b526eb
zahraTaha <143745758+zahraTaha@users.noreply.github.com>
2023-09-27 15:37:38 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,12 @@
 // JavaScript Document
 console.log("hi");
 
+var menuButton = document.querySelector("header button");
+var deNav = document.querySelector("header nav");
 
+
+menuButton.onclick = toggleMenu;
+
+function toggleMenu() {
+	deNav.classList.toggle("open");
+}
